693805106835d6931d6e1222e21f6a2dd38a1d45
Omar Fahmy <Ofahmy1234@gmail.com>
2022-08-23 04:43:14 +0200

    Add the storing of buttons to be selected functionality for the simon game.

--- index.js
+++ index.js
@@ -1,40 +1,131 @@
 
 
-
+// counter for the levels
 var levelCounter = 1 ;
-
-
-
+// array to store the buttons to be selected
+var arrToBeSelected = [] ;
+// variable to store the current pressed button
+var buttonPressed ;
+// flag for game over
+var end = false;
+// counter for the to be selected buttons array
+var counter = 0;
+//cariable to store the blinked over button to be stored in the to be selected buttons array
 var checker = "String";
 
 document.addEventListener("keypress" , function(event){
+    // checks if it's game over to reload
+    if(end == true){
+        location.reload();
+    }else{
+        // if it's not game over it just starts the game by starting the blinker
     var startSound = new Audio("sounds/start.wav");
     startSound.play();
     $("h1").text("Level "+levelCounter);
     checker = randomSelector();
+    arrToBeSelected.push(checker);
+}
 })
 
 
-// set animation and sound on button click
 $("button").click(function(event){
+    // store the id of the selected button
     buttonPressed = event.target.id;
+    // check the selected button with the corresponding item in the array
+    if(buttonPressed === arrToBeSelected[counter]){
+        //to be selected array counter increases
+        counter++;
+        // check if this is the last element of the array which means we got all the buttons right
+        if(counter === levelCounter) {
+            //go on to the next level
+            levelCounter++;
+            //update the header
+            $("h1").text("level "+levelCounter);
+            // console.log("win");
+            // set the blinker to work after 1 second to avoid confusion
+            setTimeout(function(){
+                checker = randomSelector();
+                arrToBeSelected.push(checker);
+                counter = 0;
+                console.log(arrToBeSelected);
+            },1000)
 
+        }
 
-    if(checker === buttonPressed){
-        console.log("win");
-        levelCounter ++;
-        $("h1").text("Level "+levelCounter);
-        setTimeout(function(){
-            checker = randomSelector();
+    }else {
+        //Game over screen changes
+        //change the header
+        $("h1").text("Game Over press any key to try again ");
+        //remove the buttons while giving th eoption to reload via keyboard
+        $("button").fadeOut();
+        //game over buzz
+        var GameOverSound = new Audio("sounds/wrong-answer-sound-effect.mp3");
+        GameOverSound.play();
 
-            console.log("wait");
-        },2000);
-
+        end = true;
     }
+
+    // set animation and sound on button click
     buttonPressedAnimation(event.target.id , 100);
     buttonSound(event.target.id);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // buttonPressed.push(event.target.id.toString())     
+    // if(checker.toString() === buttonPressed.toString()){
+    //     console.log("win");
+    //     levelCounter ++;
+    //     $("h1").text("Level "+levelCounter);
+    //     setTimeout(function(){
+    //         checker = randomSelector();
+    //         arr.push(checker);
+    //         console.log("wait");
+    //         console.log(arr);
+    //         console.log(buttonPressed);
+    //     },1000);
+
+    // }else{
+    //     buttonPressed = [];
+    //     var GameOverSound = new Audio("sounds/wrong-answer-sound-effect.mp3");
+    //     GameOverSound.play();
+    //     $("body").addClass("GameOver");
+    //     setTimeout(function(){
+    //         $("html").removeClass("GameOver");
+
+    //     },1000)
+    //     $("h1").text("Game over press any key to try again");
+    //     $("button").fadeOut();
+    //     end = true;
+
+    // }
+
+
 });
+
+// the blinker function that blinks at buttons to be selected
 
 function randomSelector(){
 
@@ -49,7 +140,7 @@ function randomSelector(){
     return randomButton;
 }
 
-
+// adding the blinking animations to the buttons
 function buttonPressedAnimation(buttonId , delay){
     $("#" + buttonId).addClass("pressed");
     setTimeout(function(){
@@ -57,7 +148,7 @@ function buttonPressedAnimation(buttonId , delay){
     },delay);
 }
 
-
+// adding the sounds to the buttons
 function buttonSound(button){
     switch(button){
         case "green":
@@ -89,7 +180,7 @@ function buttonSound(button){
 
 
 
-var win = true ; 
+// var win = true ; 
 // console.log(gameStart);
 
 
